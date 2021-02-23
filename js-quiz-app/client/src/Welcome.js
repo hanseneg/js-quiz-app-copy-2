534fd59e5fa153bbb1e3aed6b26aa3c6033fcaa4
Sean Joseph <sean_joseph@protonmail.com>
2021-02-22 18:28:20 -0800

    Adding Welcome and Leaderboard

--- js-quiz-app/client/src/Welcome.js
+++ js-quiz-app/client/src/Welcome.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import {useState} from 'react';
+import {useHistory} from "react-router-dom"
 
 function Welcome(props){
+    const [user, setUser] = useState("")
+    let history = useHistory();
+
+
+    const handleChange = (e) => {
+        setUser(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(user);
+        history.push("/questions");
+        
+    }
+
  return (
-     <h1>Welcome</h1>
+     <div>
+         <h1>Welcome to the JavaScript Quiz</h1>
+         <h2>Enter your name to begin.</h2>
+
+         <form name="user-form" onSubmit={handleSubmit}>
+             <input
+             type="text"
+             name="user"
+             onChange={handleChange}
+             placeholder="Enter your name"
+             value={user}
+             >
+             </input>
+             <button>Submit and Start Game!</button>
+
+         </form>
+     </div>
+     
+
  )
 }
 export default Welcome;
